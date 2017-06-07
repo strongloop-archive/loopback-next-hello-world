732c1576469aff3de173ce334b2acc56a99c5d72
emckean <emckean@Erins-MBP-2.usca.ibm.com>
2017-06-07 14:18:34 -0700

    Update index.ts to add server + gitignore

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,20 @@
+
 import {createApp} from './application';
-// import {http} from 'http';
-// import {https} from 'https';
+import * as http from 'http';
+import * as https from 'https';
 //import {handleHttp, handleHttps} from '@loopback/handlers';
 
 const app = createApp();
 
 import {HelloWorldController} from './components/controllers/hello-world';
 app.controller(HelloWorldController);
+
+function createHttpHandler(app) {
+  return (req, res) => {
+    app.handleHttp(req, res)
+  }
+}
+http.createServer(createHttpHandler(app)).listen(8080)
 
 //http.createServer(handleHttp(app, {httpsRedirect: true})).listen(80);
 //https.createServer(handleHttps(app)).listen(443);
