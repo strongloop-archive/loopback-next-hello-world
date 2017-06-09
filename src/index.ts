4af5379ef09e359e9a443664ecc71fdb91e9d325
Simon Ho <simonho@ca.ibm.com>
2017-06-09 00:15:40 -0700

    Add follow up steps to starting server

--- src/index.ts
+++ src/index.ts
@@ -10,4 +10,5 @@ const server = http.createServer(app.handleHttp);
 server.listen(3000, (err) => {
   if (err) throw err;
   console.log(`HTTP server listening on port ${3000}`);
+  console.log('Run `curl localhost:3000/helloworld?name=YOUR_NAME` to try it out');
 });
