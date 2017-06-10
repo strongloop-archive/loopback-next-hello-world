7d66d1707202ec497d8410bfee53d2c8be000fb6
Simon Ho <simonho@ca.ibm.com>
2017-06-09 18:14:48 -0700

    Add updates from @loopback/core 4.0.0-alpha.8

--- src/application.ts
+++ src/application.ts
@@ -4,7 +4,6 @@ import {HelloWorldComponent} from './components/hello-world';
 export function createApp(): Application {
     return new Application({
         components: [HelloWorldComponent],
-        sequences: []
     });
 }
 
