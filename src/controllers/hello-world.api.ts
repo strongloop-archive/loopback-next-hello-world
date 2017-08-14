00af21eea9b53e4495ca0ed0a7a9df736129d6a2
Raymond Feng <rfeng@us.ibm.com>
2017-08-14 16:28:57 -0700

    Fix the api spec to avoid validation errors

--- src/controllers/hello-world.api.ts
+++ src/controllers/hello-world.api.ts
@@ -1,30 +1,27 @@
 export const def = {
-    basePath: '/',
-    paths: {
-        '/helloworld': {
-            get: {
-                'x-operation-name': 'helloWorld',
-                parameters: [
-                    {
-                        name: 'name',
-                        in: 'query',
-                        description: 'Your name.',
-                        required: false,
-                        type: 'string',
-                    },
-                ],
-                responses: {
-                    200: {
-                        schema: {
-                            response: {
-                                type: 'string',
-                                description: 'Returns a hello world with your (optional) name.',
-                            },
-                        },
-                    },
-                },
-            },
-        },
-    },
+  basePath: '/',
+  paths: {
+    '/helloworld': {
+      get: {
+        'x-operation-name': 'helloWorld',
+        parameters: [
+          {
+            name: 'name',
+            in: 'query',
+            description: 'Your name',
+            required: false,
+            type: 'string'
+          }
+        ],
+        responses: {
+          200: {
+            description: 'Hello message response',
+            schema: {
+              type: 'string'
+            }
+          }
+        }
+      }
+    }
+  }
 };
-
