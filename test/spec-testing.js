701215db3f518f43b4e5fb7cc09a113d41bac699
Tetsuo Seto <tseto@us.ibm.com>
2017-08-17 20:25:46 -0700

    Incorporate review comments

--- test/spec-testing.js
+++ test/spec-testing.js
@@ -4,10 +4,11 @@
 // License text available at https://opensource.org/licenses/MIT
 'use strict';
 
+const Dredd = require('dredd');
 const expect = require('@loopback/testlab').expect;
 const HelloWorldApp = require('../lib/application').HelloWorldApp;
 const http = require('http');
-const Dredd = require('dredd');
+
 const port = 3030; // use this port for Dredd testing only
 const localhost = 'http://localhost:' + port;
 const dreddConfig = {
@@ -15,19 +16,22 @@ const dreddConfig = {
   options: {
     level: 'verbose',
     silent: true, // false for helpful debugging info
-    path: [localhost + '/openapi.json'], // to download our apiSpec
+    path: [localhost + '/openapi.json'], // to download apiSpec from the service
   }
 };
 
 describe('Api Spec End Points', () => {
-  let app;
-  let dredd;
   let server;
   let error;
   let ctFailures;
   beforeEach(done => {
-    initEnvironment(dreddConfig);
-    testApi(done);
+    initEnvironment(dreddConfig, (dredd) => {
+      testApi(dredd, done);
+    });
+  });
+  afterEach(done => {
+    server.close();
+    done();
   });
 
   describe('Test end points', () => {
@@ -37,29 +41,24 @@ describe('Api Spec End Points', () => {
     });
   });
 
-  function testApi(done) {
+  function testApi(dredd, done) {
     dredd.run((err, stats) => {
       // err is present if anything went wrong and stats is nill
       error = err;
       // otherwise stats is an object with useful statistics
       ctFailures = failureCount(stats);
-      server.close();
       done();
     });
   }
 
-  function initEnvironment(config) {
-    app = new HelloWorldApp();
-    server = startApp(app);
-    dredd = new Dredd(config);
-  }
-
-  function startApp(app) {
-    const server = http.createServer(app.handleHttp);
-    server.listen(port, (err) => {
+  async function initEnvironment(config, next) {
+    const app = new HelloWorldApp();
+    const svr = http.createServer(app.handleHttp);
+    svr.listen(port, (err) => {
       if (err) throw err;
+      server = svr;
+      next(new Dredd(config));
     });
-    return server;
   }
 
   function failureCount(stats) {
